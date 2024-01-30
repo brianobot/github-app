21a15bc73e6486e1b0644362657955b49afc703e
Brian Obot <brianobot9@gmail.com>
2024-01-30 08:33:31 +0100

    Update progress

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
 import environ
+from utils import get_jwt_token
 
 env = environ.Env()
 environ.Env.read_env()
@@ -7,3 +8,14 @@ environ.Env.read_env()
 APP_ID = env("GITHUB_APP_ID")
 CLIENT_ID = env("GITHUB_CLIENT_ID")
 CLIENT_SECRET = env("GITHUB_CLIENT_SECRET")
+
+def get_headers(token: str) -> dict:
+    return {
+        'X-GitHub-Api-Version': '2022-11-28',
+        'Accept': 'application/vnd.github+json',
+        'Authorization': f'Bearer {token}',
+    }
+
+token = get_jwt_token()
+
+
