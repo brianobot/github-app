8bc5070bf502f14f03effbd0855978649b6588d9
Brian Obot <brianobot9@gmail.com>
2024-08-18 19:36:19 +0100

    Improve the codebase and complete the get following functino

--- app.py
+++ app.py
@@ -1,21 +1,90 @@
 import requests
 import environ
-from utils import get_jwt_token
+from functools import partial
 
 env = environ.Env()
 environ.Env.read_env()
 
-APP_ID = env("GITHUB_APP_ID")
-CLIENT_ID = env("GITHUB_CLIENT_ID")
-CLIENT_SECRET = env("GITHUB_CLIENT_SECRET")
+ACCESS_TOKEN = env("GITHUB_ACCESS_TOKEN")
+ACCESS_TOKEN = "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"
 
-def get_headers(token: str) -> dict:
+
+def get_headers() -> dict:
     return {
-        'X-GitHub-Api-Version': '2022-11-28',
-        'Accept': 'application/vnd.github+json',
-        'Authorization': f'Bearer {token}',
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"token {ACCESS_TOKEN}",
     }
 
-token = get_jwt_token()
+
+def get_user_attribute(attr: str, params: dict | None = None) -> dict:
+    url = f"https://api.github.com/user/{attr}"
+    response = requests.get(url, headers=get_headers(), params=params)
+    return response.json()
 
 
+def check_rate_limit() -> dict:
+    url = "https://api.github.com/rate_limit"
+    response = requests.get(url, headers=get_headers())
+    return response.json()
+
+
+def get_profile_url(profile_name: str) -> str:
+    return f"https://github.com/{profile_name}"
+
+
+def get_all(
+    func: callable, params: dict, key: list[str] = ["login", "followers_url"]
+) -> list:
+    page = valid = True
+    attrs = []
+    while valid:
+        attr = func(params={**params, "page": page})
+        new_attr = [
+            f"{each['login']}:{get_profile_url(each['login'])}" for each in attr
+        ]
+        attrs.extend(new_attr)
+        if len(attr) < params.get("per_page", 30):
+            valid = False
+
+        print(f"Page {int(page)} - Length: {len(new_attr)}")
+        page += 1
+    return attrs
+
+
+def write_to_file(attrs: list, filename: str) -> int:
+    with open(f"{filename}.txt", "w") as file:
+        for name in attrs:
+            file.write(name + "\n")
+        file_no = file.fileno()
+    return file_no
+
+
+def compare_files(file1: str, file2: str):
+    file1_data = set(open(file1).readlines())
+    file2_data = set(open(file2).readlines())
+
+    return file2_data - file1_data
+
+
+def main():
+    get_followers_func = partial(get_user_attribute, "followers")
+    get_followings_func = partial(get_user_attribute, "following")
+
+    all_followers = get_all(get_followers_func, params={"per_page": 100})
+    all_followings = get_all(get_followings_func, params={"per_page": 100})
+
+    # write_to_file(all_followers, 'all-followers')
+    # write_to_file(all_followings, 'all-followings')
+
+    # diffs = compare_files('all-followers.txt', 'all-followings.txt')
+    # diffs = [diff.strip() for diff in diffs]
+
+    diffs = set(all_followings) - set(all_followers)
+
+    write_to_file(list(diffs), "not-following-back")
+
+    print("Rate = ", check_rate_limit())
+
+
+if __name__ == "__main__":
+    main()
